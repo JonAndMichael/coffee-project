465a175ce2bdac048bddb3829cd46da7b0ef966a
Michael Jones <michael.k.jones.20@gmail.com>
2022-01-14 12:34:00 -0600

    Project update. Need to fix empty localStorage system

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 
 "use strict"
 
+function buildCoffees(coffees) {
+    for(let i = 1; i <= localStorage.coffeeCount; i++ )
+    {
+        let coffee = localStorage.getItem("" + i).split(',');
+        let tempCoffee = {
+            id: i,
+            name: coffee[0],
+            roast: coffee[1]
+        }
+        coffees.push(tempCoffee);
+    }
+    console.log(coffees);
+}
+
 function renderCoffee(coffee) {
     var html = '<div class="d-inline-block col-6 my-2">';
     html += '<h1 class="d-inline-block">' + coffee.name + '</h1>';
     html += '<p class="d-inline-block text-secondary fs-4 px-2">' + coffee.roast + '</p>';
     html += '</div>';
-
     return html;
 }
 
-// function renderCoffees(coffees) {
-//     var html = '';
-//     for(var i =0; i < coffees.length; i++) {
-//         html += renderCoffee(coffees[i]);
-//     }
-//
-//
-//     return html;
-// }
-
 function renderCoffees(coffees) {
     var html = '';
-    coffees.forEach(function (coffee, idx) {
-        console.log(localStorage.getItem("" + (idx +1)));
-    });
-
+    for(var i =0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
     return html;
 }
 
@@ -68,32 +70,18 @@ function addCoffee(e) {
     var filteredCoffees = [];
     coffees.forEach(function (coffee) { filteredCoffees.push(coffee); });
     tbody.innerHTML = renderCoffees(filteredCoffees);
+
     localStorage.coffeeCount = coffees.length;
     console.log(localStorage.coffeeCount);
     coffees.forEach(function (coffee, idx) {
-        localStorage.setItem("" + (idx +1), coffee.name + " " + coffee.roast);
+        localStorage.setItem("" + (idx +1), coffee.name + "," + coffee.roast);
     });
 
 
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+var coffees = [];
 
 var tbody = document.querySelector('#coffees');
 var submitSearchButton = document.querySelector('#submit-search');
@@ -103,6 +91,7 @@ var submitAddButton =  document.querySelector('#submit-add');
 var coffeeAddNameField = document.querySelector('#name');
 var roastSelectionAdd = document.querySelector('#roast-selection-add');
 
+buildCoffees(coffees)
 tbody.innerHTML = renderCoffees(coffees);
 
 submitSearchButton.addEventListener('click', searchCoffees);
